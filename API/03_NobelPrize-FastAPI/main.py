426dd7974d447afd27945c2878a5c0febc7413c0
Alireza Kiaeipour <a.kiaipoor@gmail.com>
2024-03-20 04:25:59 +0330

    Nobel Prize

---
+++ API/03_NobelPrize-FastAPI/main.py
@@ -0,0 +1,31 @@
+import io
+import json
+import cv2
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import StreamingResponse 
+
+app = FastAPI()
+data = data = json.load(open("nobel.json"))
+
+@app.get("/nobel")
+def read_nobel():
+    return data["nobel_info"]
+
+
+@app.get("/nobel/{categories}")
+def read_categories(categories:str):
+    if categories.lower() in data:
+        return data[categories.lower()]
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Please enter the correct value: 1-physics  2-chemistry  3-medicine  4-literature")
+
+
+@app.get("/image/{image_categories}")
+def read_image(image_categories:str):
+    if image_categories.lower() in data["image"]:
+        image = cv2.imread(data["image"][image_categories.lower()])
+        _,encoded_image = cv2.imencode(".png",image)
+        image = StreamingResponse(io.BytesIO(encoded_image.tobytes()),media_type="image/png")
+        return image
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Please enter the correct value: 1-physics  2-chemistry  3-medicine  4-literature")
